1b952524acad706f48ba89bb23e96e90d9ecb3a5
JGlow <ntfr123@gmail.com>
2025-03-05 00:31:41 +0100

    add king class

---
+++ king.cpp
@@ -0,0 +1,36 @@
+#include "king.h"
+
+King::King(Color color) : Figure(color) {}
+
+QVector<QString> King::availableMoves(const QString& position) const {
+    QVector<QString> moves;
+
+    // Get the file and rank from the position
+    char file = position[0].toLatin1();
+    int rank = position[1].digitValue();
+
+    // Define possible moves (relative to the current position)
+    QVector<QPair<int,int>> moveOffsets = {
+        {-1, -1}, {-1, 0}, {-1, 1},
+        {0, -1},          {0, 1},
+        {1, -1}, {1, 0}, {1, 1}
+    };
+
+    // Iterate through possible moves
+    for (const auto& offset : moveOffsets) {
+        // Calculate the new file and rank
+        char newFile = file + offset.first;
+        int newRank = rank + offset.second;
+
+        // Check if the new position is within the board bounds
+        if (newFile >= 'a' && newFile <= 'h' && newRank >= 1 && newRank <= 8) {
+            moves.push_back(QString(QChar(newFile)) + QString::number(newRank));
+        }
+    }
+
+    return moves;
+}
+
+QString King::getTypeString() const {
+    return "king";
+}
---
+++ king.h
@@ -0,0 +1,17 @@
+#ifndef KING_H
+#define KING_H
+
+#include "figure.h"
+#include <QVector>
+#include <QString>
+
+class King : public Figure {
+public:
+    King(Color color);
+    ~King() override = default;
+
+    QVector<QString> availableMoves(const QString& position) const override;
+    QString getTypeString() const override;
+};
+
+#endif // KING_H
